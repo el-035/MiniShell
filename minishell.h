e623d3c2ccfe3fa1bdcf0c5382e616b23b391446
el-035 <elisa.fittant@gmail.com>
2025-03-11 16:37:00 +0100

    added structs for parsing

--- minishell.h
+++ minishell.h
@@ -9,4 +9,30 @@
 # include <sys/wait.h>
 # include <unistd.h>
 
+typedef enum e_type
+{
+	CMD,         // Command (e.g., "ls", "cat")
+	ARG,         // Argument (e.g., "-a", "file.txt")
+	PIPE,        // Pipe ("|")
+	REDIR_IN,    // Input redirection ("<")
+	REDIR_OUT,   // Output redirection (">")
+	REDIR_APPEND,// Append redirection (">>")
+	HERE_DOC,    // Here document ("<<")
+	ENV_VAR,     // Environment variable ("$HOME")
+	S_QUOTE,     // Quoted string ('...' or "...")
+	D_QUOTE,	 // Quoted string ('...' or "...")
+	SEP,         // Separator (e.g., ";")
+	OPERATOR,    // Logical operators ("&&", "||")
+	SUBSHELL     // Subshell ("(cmd)")
+}	t_type;
+
+typedef struct	s_input
+{
+	char 		*content;
+	t_type		*type;
+	int			position;
+	t_input		*next;
+	t_input		*prev;
+}				t_input
+
 # endif
