b4461c15ccfd99d44dd7a85702167115464f5a64
Elisa Fittante <efittant@c3r6p4.42vienna.com>
2025-06-22 19:26:52 +0200

    exit done

--- ft_exit.c
+++ ft_exit.c
@@ -1,32 +1,4 @@
 #include "minishell.h"
-#include <limits.h>
-
-long	long_atoi(const char *str)
-{
-	int		i;
-	long	result;
-	int		n;
-
-	n = 1;
-	i = 0;
-	result = 0;
-	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
-		i++;
-	if (ft_strncmp(&str[i], "-9223372036854775808", 20) == 0)
-		return (LONG_MIN);
-	if (str[i] == '-' || str[i] == '+')
-	{
-		if (str[i] == '-')
-			n = -1;
-		i++;
-	}
-	if (str[i])
-	{
-		while (str[i] >= 48 && str[i] <= 57)
-			result = result * 10 + str[i++] - 48;
-	}
-	return (result * n);
-}
 
 int	error_n(char *err)
 {
@@ -61,25 +33,37 @@ char	*clean_input(char *trim)
 		clean = ft_strdup(trim);
 	return (clean);
 }
+
 int	check_overflow(char *trimmed)
 {
 	char	*converted;
 	char	*clean;
 
-
 	clean = clean_input(trimmed);
 	if (!clean)
 		return (fail_mall(), free(trimmed), -2);
-
-
 	converted = ft_itoa(long_atoi(clean));
 	if (!converted)
 		return (fail_mall(), free(trimmed), free(clean), -2);
-
-
-	if (ft_strncmp(converted, clean, ft_strlen(converted)) != 0)		//vehck thias
+	if (ft_strncmp(converted, clean, ft_strlen(converted)) != 0) // vehck thias
 		return (free(converted), free(clean), -1);
 	return (free(converted), free(clean), 1);
+}
+
+void	print_err(char *str, int flag)
+{
+	if (flag == 1)
+	{
+		write(2, "bash: exit: ", 12);
+		write(2, str, ft_strlen(str));
+		write(2, ": numeric argument required\n", 28);
+		return_exit_code(2);
+	}
+	else if (flag == 2)
+	{
+		write(2, "exit: too many arguments\n", 25);
+		return_exit_code(1);
+	}
 }
 
 void	ft_exit(t_data *data, t_cmd *cmd)
@@ -92,29 +76,18 @@ void	ft_exit(t_data *data, t_cmd *cmd)
 	{
 		trimmed = ft_strtrim(cmd->args[1], " \t");
 		if (!trimmed)
-			return (fail_mall(), free_split(data->envp), free_all(data), exit(1));
-		
+			return (fail_mall(), free_split(data->envp), free_all(data),
+				exit(1));
 		is_valid = check_overflow(trimmed);
 		if (is_valid == -2)
 			return (free_split(data->envp), free_all(data), exit(1));
-
-
 		if (ft_str_digit(trimmed) != 0 || is_valid == -1)
-		{
-			write(2, "bash: ", 6);
-			write(2, "exit: ", 6);
-			write(2, cmd->args[1], ft_strlen(cmd->args[1]));
-			write(2, ": numeric argument required\n", 28);
-			return_exit_code(2);
-		}
+			print_err(cmd->args[1], 1);
 		else if (cmd->args[2] && ft_str_digit(cmd->args[1]) == 0)
-		{
-			write(2, "exit: too many arguments\n", 25);
-			return_exit_code(1);
-		}
+			print_err(cmd->args[1], 2);
 		else
 			error_n(cmd->args[1]);
-		free (trimmed);
+		free(trimmed);
 	}
 	return (free_split(data->envp), free_all(data), exit(return_exit_code(-2)));
 }
