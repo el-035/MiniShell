ca2e424522f54600caa0faa105c41f2d47167bdd
Elisa Fittante <efittant@c3r6p1.42vienna.com>
2025-06-27 16:05:42 +0200

    fixed small thing in syntax

--- syntax.c
+++ syntax.c
@@ -24,10 +24,13 @@ int	more_syntax(t_input *cur)
 
 int	check_nl(t_input *first)
 {
-	if (first->type == REDIR_IN || first->type == HERE_DOC)
+/* 	if (first->type == REDIR_IN || first->type == HERE_DOC
+		&& !first->next)
 		return (syntax_err(), 1);
 	if ((first->type == REDIR_APPEND || first->type == REDIR_OUT)
 		&& !first->next)
+		return (syntax_err(), 1); */
+	if (is_red(first) == 1 && !first->next)
 		return (syntax_err(), 1);
 	if (first->type == PIPE)
 		return (syntax_err(), 1);
