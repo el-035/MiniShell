847935debb91c077e7b3d56a6923b726eddadafe
TheTalkingFox <63100439+TheTalkingFox@users.noreply.github.com>
2025-07-05 15:11:35 +0000

    42 headers fix

--- error_handler.c
+++ error_handler.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handler.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apchelni <apchelni@student.42vienna.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 00:54:42 by apchelni          #+#    #+#             */
+/*   Updated: 2025/04/18 17:48:36 by apchelni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	handle_error(char *str, int error_code)
@@ -6,13 +18,11 @@ void	handle_error(char *str, int error_code)
 	{
 		write(2, str, ft_strlen(str));
 		write(2, ": No such file or directory\n", 28);
-	//	return_exit_code(1);
 	}
 	else if (error_code == 1)
 	{
 		write(2, str, ft_strlen(str));
 		write(2, ": Permission denied\n", 20);
-	//	return_exit_code(1);
 	}
 	else if (error_code == 2)
 	{
