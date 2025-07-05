2ed6c31c450fdeb593026a6d74204b1ef02be6df
Elisa Fittante <efittant@c2r5p4.42vienna.com>
2025-07-05 16:27:55 +0200

    export work in progress

--- ft_export.c
+++ ft_export.c
@@ -1,19 +1,27 @@
 #include "minishell.h"
 
-int	ft_is_valid(char *str)
+int	ft_is_valid(char *cmd)
 {
 	int	i;
 
 	i = 0;
-	if (!ft_isalpha(str[i]) && str[i] != '_')
+	if (!ft_isalpha(cmd[i]) && cmd[i] != '_')
 		return (1);
 	i++;
-	while (str[i])
+	while (cmd[i] && cmd[i] != '=' && cmd[i] != '+')
 	{
-		if (!ft_isalnum(str[i]) && str[i] != '_')
+		if (!ft_isalnum(cmd[i]) && cmd[i] != '_')
 			return (1);
 		i++;
 	}
+	if (cmd[i] == '+')
+	{
+		if (!cmd[i + 1] || cmd[i + 1] != '=')
+			return (1);
+		i++;
+	}
+	if (cmd[i] != '=')
+		return (1);
 	return (0);
 }
 
@@ -35,7 +43,7 @@ int	find_var(char **envp, char *str)
 	free(var);
 	return (-1);
 }
-
+/* 
 char	*append_replace(char *arg, char *content, char *prev)
 {
 	char	*tmp;
@@ -54,9 +62,9 @@ char	*append_replace(char *arg, char *content, char *prev)
 		free(prev);
 	}
 	return (app);
-}
+} */
 
-void	export_helper(t_data *data, t_cmd *cmd, int i, char *var)
+/*  void	export_helper(t_data *data, t_cmd *cmd, int i, char *var)
 {
 	char	*content;
 	char	*tmp;
@@ -83,13 +91,79 @@ void	export_helper(t_data *data, t_cmd *cmd, int i, char *var)
 		add_env(data, var, content);
 			// not printinf error message but otherwise too many lines
 	return (free(content));
+}  */
+
+int append(char *var, char *cmd, t_data *data)
+{
+	char *new;
+	char *old;
+	char *app;
+	int	j;
+
+	j = find_var(data->envp, var);
+	new = get_content(cmd);
+	if (!new)
+		return (fail_mall(), -1);
+	old = get_content(data->envp[j]);
+	if (!old)
+		return (free(new), fail_mall(), -1);
+	app = ft_strjoin(old, new);
+	free(new); free(old);
+	if (!app)
+		return (fail_mall(), -1);
+	new = double_join(var, "=", app);
+	if (!new)
+		return (free(app), fail_mall(), -1);
+	free(data->envp[j]);
+	data->envp[j] = new;
+	return (free(app), 0);
+}
+
+int replace(char *var, char *cmd, t_data *data)
+{
+	char *content;
+	int j;
+	char *new;
+
+	j = find_var(data->envp, var);
+	content = get_content(cmd);
+	if (!content)
+		return (fail_mall(), -1);
+	new = double_join(var, "=", content);
+	if (!new)
+		return (free(content), fail_mall(), -1);
+	free(data->envp[j]);
+	data->envp[j] = new;
+	return (free(content), 0);
+}
+
+int export_helper(char *args, t_data *data, char *var)
+{
+	/* char *tmp; */
+
+	/* tmp = ft_strchr(args, '='); */
+	if (find_var(data->envp, var) == -1)
+	{
+		if (add_env(data, var, get_content(args)) == -1)
+			return (fail_mall(), -1); 
+	}
+	else if (args[ft_strlen(var)] == '+' )
+	{
+		if (append(var, args, data) == -1)
+			return (free(var), -1);
+	}
+	else
+	{
+		if (replace(var, args, data) == -1)
+			return (free(var), -1);
+	}
+	return (0);
 }
 
 void	ft_export(t_data *data, t_cmd *cmd)
 {
 	int		i;
 	char	*var;
-//	char *tmp;
 
 	i = 0;
 	if (!cmd->args[1])
@@ -99,23 +173,18 @@ void	ft_export(t_data *data, t_cmd *cmd)
 		var = get_var(cmd->args[i]);
 		if (!var)
 			return (fail_mall());
-		if (ft_is_valid(var) != 0)
+		if (ft_is_valid(cmd->args[i]) != 0)
 		{
 			write(2, "bash: export: `", 15);
 			write(2, cmd->args[i], ft_strlen(cmd->args[i]));
 			write(2, "': not a valid identifier\n", 26);
 			return_exit_code(1);
-			/* write(2, "bash: export: `", 15);
-			tmp = ft_strdup(cmd->args[i]);
-				//protect;
-			remove_useless_quotes(&tmp, return_final_len(tmp));
-			write(2, tmp, ft_strlen(tmp));
-			write(2, "': not a valid identifier\n", 26);
-			free(tmp);
-			return_exit_code(1); */
 		}
 		else
-			export_helper(data, cmd, i, var);
+		{
+			if (export_helper(cmd->args[i], data, var) == -1)	//protect
+				return (free(var));
+		}
 		free(var);
 	}
 }
